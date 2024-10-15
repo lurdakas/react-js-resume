f35a51215e5fc458a094dc1f7a654ae71f6e9938
lurdakas <lkasileviciute@protonmail.com>
2024-10-15 15:29:56 +0300

    new day

--- src/Pages/Home/ContactMe.jsx
+++ src/Pages/Home/ContactMe.jsx
@@ -4,7 +4,6 @@ export default function ContactMe(){
             <div>
                 <p className="sub--title"> Get In Touch</p>
                 <h2>Contact Me</h2>
-                <p className="text-lg"> alifhjlakdsjflkadsjflasd</p>
             </div>
             <form className="contact--form--container">
             <div className="container">
